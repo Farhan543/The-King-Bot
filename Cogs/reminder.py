32056dbc9f61890758dc88889235e1c3998d690f
Farhan <farhan.haneef03@gmail.com>
2023-04-14 14:44:44 +0530

    Updated reminder command, can add reminders

--- Cogs/reminder.py
+++ Cogs/reminder.py
@@ -9,17 +9,20 @@ class reminder(commands.Cog):
         self.client =client
 
     @app_commands.command(name="reminder",description="Adding a reminder.")
-    async def reminder(self,interaction:discord.Interaction,message:str,day:int,month:int,year:int,hour:int,minute:int,second:int):
-
-        current_date = date.today()
-        today_date = date(current_date.year,current_date.month,current_date.day)
-        reminder_date = date(year,month,day)
-        total_days = (reminder_date - current_date).days
-        seconds = (hour * 60 * 60) + (minute * 60) +second
-        total_seconds = (total_days * 24 * 60 * 60) + seconds
+    async def reminder(self,interaction:discord.Interaction,message:str,future_date:str,future_time:str):
+        current_time = datetime.now()
+        curent_time_str = current_time.strftime("%Y-%m-%d %H:%M:%S")
+        date_one = datetime.strptime(curent_time_str,"%Y-%m-%d %H:%M:%S")
+        future_rem = future_date + " " + future_time
+        date_two = datetime.strptime(future_rem,"%Y-%m-%d %H:%M:%S")
+        delta = date_two - date_one
+        total_seconds = delta.total_seconds()
+        member = interaction.user
         await interaction.channel.send("Reminder has been set")
         channel = self.client.get_channel(int('1095015393137004554'))
         await asyncio.sleep(total_seconds)
-        await channel.send(content=f"{message}")
+        await channel.send(content=f"**{member.mention} Here is your reminder -** {message}")
+
+        
 async def setup(client:commands.Bot) -> None:
     await client.add_cog(reminder(client))
